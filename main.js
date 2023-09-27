2295f27a4e9a45ecd3b43c560193f8fde2493b7a
mikehwebdev <81847609+mikehwebdev@users.noreply.github.com>
2023-09-27 12:16:44 +0100

    Ready for online testing

--- main.js
+++ main.js
@@ -34,11 +34,60 @@ let maxWeightString = ''
 
 const inputs = document.querySelector('.details-capture')
 
-inputs.addEventListener('input', lengthCheck)
+// Event listeners
+
+
+// These event listeners toggle the metric or imperial view for inputting your measurements
+
+imperialRadioEl.addEventListener('click', () => {
+    imperialInputsEl.style.display = 'grid'
+    metricInputsEl.style.display = 'none'
+})
+
+metricRadioEl.addEventListener('click', () => {
+    imperialInputsEl.style.display = 'none'
+    metricInputsEl.style.display = 'grid'
+})
+
+// THe rest of these event listeners trigger if data has been entered into the input fields. This then runs either the imperialUpdater function or metricUpdater function to ensure all fields are entered. This ensures that the BMI result doesn't change constantly while the user is entering their specifics and makes for a better user experience. If all input fields have a value the main bmiCalculator function executes with the parameter of which measurement system is being entered and the BMI is calculated accordingly.  
+
+metricHeightEl.addEventListener('keyup', () => {
+    metricHeightValue = metricHeightEl.value
+    metricUpdater() 
+})
+
+metricWeightEl.addEventListener('keyup', () => {
+    metricWeightValue = metricWeightEl.value
+    metricUpdater()
+})
+
+imperialHeightFeetEl.addEventListener('keyup', () => {
+    imperialFeetValue = imperialHeightFeetEl.value
+    imperialUpdater()
+})
+
+imperialHeightInchesEl.addEventListener('keyup', () => {
+    imperialInchesValue = imperialHeightInchesEl.value
+    imperialUpdater()
+})
+
+imperialWeightStonesEl.addEventListener('keyup', () => {
+    imperialStonesValue = imperialWeightStonesEl.value
+    imperialUpdater()
+})
+
+imperialWeightPoundsEl.addEventListener('keyup', () => {
+    imperialPoundsValue = imperialWeightPoundsEl.value
+    imperialUpdater()
+})
+
+
 
 // Utility functions
 
 //The 'inputs' listener here fires when the value of input changes - ideal for my needs here. If our value exceeds 500 on any input it automatically slices the last number off. This is great for input hygiene to ensure no excessively long numbers are used which can cause visual oddities such as excessivly long results or overflowing input values.
+
+inputs.addEventListener('input', lengthCheck)
 
 function lengthCheck(e){
     let changingInputValue = e.target.value
@@ -47,24 +96,15 @@ function lengthCheck(e){
         e.target.value = changingInputValue.slice(0,-1); 
     }
 
-    if (changingInputValue.length > 5) {
-        
+    if (changingInputValue.length > 5) { 
         e.target.value = changingInputValue.slice(0,5); 
-          
      }
 }
 
-function imperialReady() {
-    if (imperialHeightFeetEl.value && imperialHeightInchesEl.value && imperialWeightPoundsEl.value && imperialWeightStonesEl.value) {
-        return true
-    } else {
-        return false
-    }
-}
-
 function imperialUpdater(){
-    if (imperialReady() === true) {
-    bmiCalculator('imperial')
+    
+    if (imperialHeightFeetEl.value && imperialHeightInchesEl.value && imperialWeightPoundsEl.value && imperialWeightStonesEl.value == true) {
+        bmiCalculator('imperial')
     } else {
         bmiInfoContainer.innerHTML = 
         `<p class="your-bmi-title">Welcome!</p>
@@ -81,6 +121,8 @@ function metricUpdater() {
         <p class="your-bmi-welcome">Enter your height and weight and you'll see your BMI result here</p>`
     }
 }
+
+// The below takes the measurement system as the parameter and runs the correct code as the calculations are very different depending on whether you input metric or imperial. Once the BMI is ascertained we can establish if the result is under, over or a healthy weight and the ideal weight range depending on the users height. We then update all the string variables and update the result section using the bmiHtmlUpdate function.
 
 function bmiCalculator(measurementSystem) {
 
@@ -142,45 +184,5 @@ function bmiHtmlUpdate() {
     `
 }
 
-// Event listeners
 
-imperialRadioEl.addEventListener('click', () => {
-    imperialInputsEl.style.display = 'grid'
-    metricInputsEl.style.display = 'none'
-})
-
-metricRadioEl.addEventListener('click', () => {
-    imperialInputsEl.style.display = 'none'
-    metricInputsEl.style.display = 'grid'
-})
-
-metricHeightEl.addEventListener('keyup', () => {
-    metricHeightValue = metricHeightEl.value
-    metricUpdater() 
-})
-
-metricWeightEl.addEventListener('keyup', () => {
-    metricWeightValue = metricWeightEl.value
-    metricUpdater()
-})
-
-imperialHeightFeetEl.addEventListener('keyup', () => {
-    imperialFeetValue = imperialHeightFeetEl.value
-    imperialUpdater()
-})
-
-imperialHeightInchesEl.addEventListener('keyup', () => {
-    imperialInchesValue = imperialHeightInchesEl.value
-    imperialUpdater()
-})
-
-imperialWeightStonesEl.addEventListener('keyup', () => {
-    imperialStonesValue = imperialWeightStonesEl.value
-    imperialUpdater()
-})
-
-imperialWeightPoundsEl.addEventListener('keyup', () => {
-    imperialPoundsValue = imperialWeightPoundsEl.value
-    imperialUpdater()
-})
 
